31aa7d6680c2f52322f2e330a0bf39e05df0f093
wordite <wordite123@gmail.com>
2025-10-19 20:37:27 +0300

    adding server search docs

--- docs/app/root.tsx
+++ docs/app/root.tsx
@@ -35,7 +35,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <body className='flex flex-col min-h-screen'>
         <QueryClientProvider client={queryClient}>
           <ReactRouterProvider>
-            <RootProvider search={{ enabled: false }} >{children}</RootProvider>
+            <RootProvider>{children}</RootProvider>
           </ReactRouterProvider>
           <ScrollRestoration />
           <script
