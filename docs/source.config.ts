8d051db9d8f38f7373dce5bbdd3e14777137d919
wordite <wordite123@gmail.com>
2025-10-20 23:52:47 +0300

    trying resolve docs image issue by change docs build config

--- docs/source.config.ts
+++ docs/source.config.ts
@@ -4,4 +4,9 @@ export const docs = defineDocs({
   dir: 'content/docs',
 });
 
-export default defineConfig();
+export default defineConfig({
+  mdxOptions: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
