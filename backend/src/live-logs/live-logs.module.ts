60bcded2afd3b03955a4d979e51c6b8e632415e9
wordite <wordite123@gmail.com>
2025-10-22 20:14:25 +0300

    fix module

--- backend/src/live-logs/live-logs.module.ts
+++ backend/src/live-logs/live-logs.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { LiveLogsService } from './live-logs.service';
 import { LiveLogsController } from './live-logs.controller';
 import { LiveLogsGateway } from './live-logs.gateway';
+import { SessionModule } from '../session/session.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [SessionModule, AuthModule],
   controllers: [LiveLogsController],
   providers: [LiveLogsService, LiveLogsGateway],
   exports: [LiveLogsService],
