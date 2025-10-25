81142c2902f0c54d64850ce8c547554ae00d82a1
wordite <wordite123@gmail.com>
2025-10-25 21:18:48 +0300

    fix error

--- backend/src/coin/coin.module.ts
+++ backend/src/coin/coin.module.ts
@@ -6,9 +6,10 @@ import { AuthModule } from '../auth/auth.module';
 import { RedisModule } from '../redis/redis.module';
 import { SolanaModule } from '../solana/solana.module';
 import { WalletModule } from '../wallet/wallet.module';
+import { AntiSpamModule } from '../anti-spam/anti-spam.module';
 
 @Module({
-  imports: [SessionModule, forwardRef(() => AuthModule), RedisModule, forwardRef(() => SolanaModule), forwardRef(() => WalletModule)],
+  imports: [SessionModule, forwardRef(() => AuthModule), RedisModule, forwardRef(() => SolanaModule), forwardRef(() => WalletModule), AntiSpamModule],
   controllers: [CoinController],
   providers: [CoinService],
   exports: [CoinService],
