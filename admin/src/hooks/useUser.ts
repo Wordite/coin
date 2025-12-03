face944833b0b84c98c4e1c50887dca76d857eef
Wordite <wordite123@gmail.com>
2025-12-03 20:00:48 +0300

    update useUser hook in admin

--- admin/src/hooks/useUser.ts
+++ admin/src/hooks/useUser.ts
@@ -2,16 +2,26 @@ import { api } from '@/app/api'
 import { useQuery } from '@tanstack/react-query'
 import type { AxiosAuthRefreshRequestConfig } from 'axios-auth-refresh'
 import { useAuthStore } from '@/app/store/authStore'
+import { AxiosError } from 'axios'
+import { Navigate } from '@/services/navigate'
+import { useEffect } from 'react'
 
 const useUser = () => {
-  const { isAuthenticated } = useAuthStore()
+  const { isAuthenticated, setIsAuthenticated } = useAuthStore()
 
-  const { data, isLoading, isError } = useQuery({
+  const { data, isLoading, isError, error } = useQuery({
     queryKey: ['user'],
     queryFn: () => api.get('/user/me', { skipAuthRefresh: true } as AxiosAuthRefreshRequestConfig),
     retry: false,
     enabled: isAuthenticated,
   })
+
+  useEffect(() => {
+    if (error instanceof AxiosError && error.response?.status === 403) {
+      setIsAuthenticated(false)
+      Navigate.to('login')
+    }
+  }, [error])
 
   return { data: data?.data, isLoading, isError }
 }
